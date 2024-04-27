6355018492c02878bf82d7bfb7c4ff0db4c39945
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2024-04-27 11:57:06 +0200

    logic

--- backend/public/script.js
+++ backend/public/script.js
@@ -1,11 +1,23 @@
 const question = document.querySelector("#question");
+const gameBoard = document.querySelector("#game-board");
+const h2 = document.querySelector("h2");
 
 function fillQuestionElements(data) {
+  if (data.winner === true) {
+    gameBoard.style.display = "none";
+    h2.innerText = "You win!";
+    return;
+  }
+  if (data.loser === true) {
+    gameBoard.style.display = "none";
+    h2.innerText = "Not this time. :-( Try again later";
+    return;
+  }
   question.innerText = data.question;
   for (const i in data.answers) {
-    const answerElement = document.querySelector(`#answer${Number(i)+1}`);
-    answerElement.innerText = data.answers[i]
-}
+    const answerElement = document.querySelector(`#answer${Number(i) + 1}`);
+    answerElement.innerText = data.answers[i];
+  }
 }
 
 function showNextQuestion() {
@@ -19,3 +31,92 @@ function showNextQuestion() {
 }
 
 showNextQuestion();
+
+const goodAnswersSpan = document.querySelector("#good-answers");
+
+function handleAnswerFeedback(data) {
+  goodAnswersSpan.innerText = data.goodAnswers;
+  showNextQuestion();
+}
+
+function sendAnswer(answerIndex) {
+  fetch(`/answer/${answerIndex}`, {
+    method: "POST",
+  })
+    .then((r) => r.json())
+    .then((data) => {
+      handleAnswerFeedback(data);
+    });
+}
+const buttons = document.querySelectorAll(".answer-btn");
+
+for (const butoon of buttons) {
+  butoon.addEventListener("click", (event) => {
+    const answerIndex = event.target.dataset.answer;
+    sendAnswer(answerIndex);
+  });
+}
+const tipDiv = document.querySelector("#tip");
+
+function handleFriendsAnswer(data) {
+  tipDiv.innerText = data.text;
+}
+
+function callToAFriend() {
+  fetch("/help/friend", {
+    method: "GET",
+  })
+    .then((r) => r.json())
+    .then((data) => {
+      handleFriendsAnswer(data);
+    });
+}
+document
+  .querySelector("#call-to-a-friend")
+  .addEventListener("click", callToAFriend);
+
+function handleHalfOnHalfAnswer(data) {
+  if (typeof data.text === "string") {
+    tipDiv.innerText = data.text;
+  } else {
+    for (const button of buttons) {
+      if (data.answersToRemove.indexOf(button.innerText) > -1) {
+        button.innerText = " ";
+      }
+    }
+  }
+}
+
+function halfOnHalf() {
+  fetch("/help/half", {
+    method: "GET",
+  })
+    .then((r) => r.json())
+    .then((data) => {
+      handleHalfOnHalfAnswer(data);
+    });
+}
+document.querySelector("#half-on-half").addEventListener("click", halfOnHalf);
+
+function handleQuestionToTheCrowd(data) {
+  if (typeof data.text === "string") {
+    tipDiv.innerText = data.text;
+  } else {
+    data.chart.forEach((percent, index) => {
+      buttons[index].innerText = `${buttons[index].innerText} : ${percent}%`;
+    });
+  }
+}
+
+function questionToTheCrowd() {
+  fetch("/help/crowd", {
+    method: "GET",
+  })
+    .then((r) => r.json())
+    .then((data) => {
+      handleQuestionToTheCrowd(data);
+    });
+}
+document
+  .querySelector("#question-to-the-crowd")
+  .addEventListener("click", questionToTheCrowd);
--- backend/routes/gameRoutes.js
+++ backend/routes/gameRoutes.js
@@ -1,8 +1,9 @@
 function gameRoutes(app) {
   let goodAnswers = 0;
+  let isGameOver = false;
   let callToAFriendUsed = false;
-  let questionToTheCrowd = false;
-  let halfOnHalf = false;
+  let questionToTheCrowdUsed = false;
+  let halfOnHalfUsed = false;
 
   const questions = [
     {
@@ -27,6 +28,10 @@ function gameRoutes(app) {
       res.json({
         winner: true,
       });
+    } else if (isGameOver) {
+      res.json({
+        loser: true,
+      });
     } else {
       const nextQuestion = questions[goodAnswers];
       const { question, answers } = nextQuestion;
@@ -36,5 +41,95 @@ function gameRoutes(app) {
       });
     }
   });
+  app.post("/answer/:index", (req, res) => {
+    if (isGameOver) {
+      res.json({
+        loser: true,
+      });
+    }
+
+    const { index } = req.params;
+
+    const question = questions[goodAnswers];
+
+    const isGoodAnswer = question.correctAnswer === Number(index);
+
+    if (isGoodAnswer) {
+      goodAnswers++;
+    } else {
+      isGameOver = true;
+    }
+
+    res.json({
+      correct: question.correctAnswer === Number(index) ? true : false,
+      goodAnswers,
+    });
+  });
+
+  app.get("/help/friend", (req, res) => {
+    if (callToAFriendUsed) {
+      return res.json({
+        text: "You've already used your call to a friend feature.",
+      });
+    }
+
+    callToAFriendUsed = true;
+
+    const doesFriendKnowAnswer = Math.random() < 0.5;
+    const question = questions[goodAnswers];
+    res.json({
+      text: doesFriendKnowAnswer
+        ? `Hmm.. For me the answer is ${
+            question.answers[question.correctAnswer]
+          }`
+        : `Hmm.. I'm not sure`,
+    });
+  });
+
+  app.get("/help/half", (req, res) => {
+    if (halfOnHalfUsed) {
+      return res.json({
+        text: "You've already used your half on half feature.",
+      });
+    }
+
+    halfOnHalfUsed = true;
+
+    const question = questions[goodAnswers];
+    const answersCopy = question.answers.filter(
+      (s, index) => index !== question.correctAnswer
+    );
+    answersCopy.splice(~~(Math.random() * answersCopy.length), 1);
+    console.log(answersCopy);
+    console.log(question);
+    res.json({
+      answersToRemove: answersCopy,
+    });
+  });
+  app.get("/help/crowd", (req, res) => {
+    if(questionToTheCrowdUsed) {
+      return res.json({
+        text: "You've already used your question to audience"
+      })
+    }
+    questionToTheCrowdUsed = true;
+    const chart = [10, 20, 30, 40];
+
+    for (let i = chart.length - 1; i > 0; i--) {
+      const change = Math.floor(Math.random() * 20 - 10);
+
+      chart[i] += change;
+      chart[i - 1] -= change;
+    }
+    const question = questions[goodAnswers];
+    const { correctAnswer} = question;
+    [chart[3], chart[correctAnswer]] = [chart[correctAnswer], chart[3]]
+
+    res.json({
+      chart,
+    });
+  });
+
 }
+
 module.exports = gameRoutes;
